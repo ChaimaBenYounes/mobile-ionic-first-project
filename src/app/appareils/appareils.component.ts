85995b0c9f66f01fafac96e1fed4bc00af2318e2
chaimaby <chaima.benyounes1@gmail.com>
2019-03-18 16:15:51 +0100

    [add] create first componenet

---
+++ src/app/appareils/appareils.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareils',
+  templateUrl: './appareils.component.html',
+  styleUrls: ['./appareils.component.scss'],
+})
+export class AppareilsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
